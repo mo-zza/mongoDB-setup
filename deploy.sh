8668892b5a60e622b0b84dbb50d4cccb4a3d8168
mo-zza <bigsmalljoe@gmail.com>
2025-08-11 15:00:18 +0900

    fix: ssh permission

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ fi
 
 # create mongodb ssl key
 ## Check if the key file already exists
-if [ -f ./ssh/mongodb.key ]; then
+if [ -f ${HOME}/.ssh/mongodb.key ]; then
     echo "MongoDB SSL key already exists."
 else
     echo "Creating MongoDB SSL key..."
-    sudo openssl rand -base64 756 > ./ssh/mongodb.key
-    sudo chmod 400 ./ssh/mongodb.key
-    cat ./ssh/mongodb.key
+    sudo openssl rand -base64 756 > ${HOME}/.ssh/mongodb.key
+    sudo chmod 400 ${HOME}/.ssh/mongodb.key
+    cat ${HOME}/.ssh/mongodb.key
     echo "MongoDB SSL key created successfully."
 fi
 
@@ -36,7 +36,7 @@ else
 fi
 
 # run mongodb with ssl
-docker compose up -d
+sudo docker compose up -d
 
 # check if mongodb is running
 docker ps | grep mongodb
